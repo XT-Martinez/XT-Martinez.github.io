4a985cbd9c39e2c1d3b6c308510d4f247b1113d1
Christian Martinez <xtian437@gmail.com>
2020-11-16 02:39:26 +0000

    Initial commit"

---
+++ build/precache-manifest.312c58e3679fd46b6063243e77777924.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba79dd6f27cebc9a9e8c8fd20a27837e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4175784db3a7026bbdb6",
+    "url": "/static/css/2.c6c44200.chunk.css"
+  },
+  {
+    "revision": "33ba762be888913f210d",
+    "url": "/static/css/main.e0388756.chunk.css"
+  },
+  {
+    "revision": "4175784db3a7026bbdb6",
+    "url": "/static/js/2.5f8523b5.chunk.js"
+  },
+  {
+    "revision": "33ba762be888913f210d",
+    "url": "/static/js/main.4395ceaf.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  },
+  {
+    "revision": "775a1144c95244e1bbbee9cc17ec0bcc",
+    "url": "/static/media/background.775a1144.jpg"
+  },
+  {
+    "revision": "9c64a2700f654740218687d3ca80203f",
+    "url": "/static/media/dp2.9c64a270.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
